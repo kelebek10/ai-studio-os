148b82e609fe059a2d7748355876449ec0581c00
kelebek10 <peyzajbalikesir@gmail.com>
2026-09-20 23:18:58 +0300

    feat: add minimal communication trigger core

---
+++ runtime/communication/__init__.py
@@ -0,0 +1,3 @@
+from .core import TaskEnvelope, TaskState, ControlDecision, create_task, authorize_problem_round
+
+__all__ = ["TaskEnvelope", "TaskState", "ControlDecision", "create_task", "authorize_problem_round"]
